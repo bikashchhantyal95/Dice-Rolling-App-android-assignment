0864b37b55e35329f5a82c3e9cba4cbf2bbc9ef5
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-04-16 17:31:07 -0400

    Created sharedpref to store dice values locally.

--- app/src/main/java/com/bikash/dicerollingapp/MainActivity.kt
+++ app/src/main/java/com/bikash/dicerollingapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.bikash.dicerollingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +18,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        retrieveDataFromSharedPref()
+
+
         val spinnerAdapter =
             ArrayAdapter(this,  android.R.layout.simple_spinner_item, listOfDice)
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
@@ -38,14 +42,33 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        binding.CreateBtn.setOnClickListener(){
+            val customDice = binding.customDiceId.text.toString()
+
+            if (customDice.isNotEmpty()){
+                if(customDice.toInt() == 0){
+                    Toast.makeText(this, "The dice cannot be zero.",Toast.LENGTH_SHORT).show()
+                }
+                if(listOfDice.contains(customDice.toInt())){
+                    Toast.makeText(this, "The dice entered is already created.",Toast.LENGTH_SHORT).show()
+                }
+                val key = listOfDice.size - 1
+                storeUserDiceData(key =key, value = customDice.toInt())
+            }
+        }
 
         binding.RollOnceBtn.setOnClickListener{
-            val dice = Dice(numberOfSides = selectedDice)
-            dice.rollDice()
-            val randomFace = dice.getRandomSideUp()
-            binding.rollTwiceOutput.visibility = View.GONE
-            binding.parentOutputId.visibility = View.VISIBLE
-            binding.rollOnceOutput.text = randomFace.toString()
+            if(selectedDice != 0){
+                val dice = Dice(numberOfSides = selectedDice)
+                dice.rollDice()
+                val randomFace = dice.getRandomSideUp()
+                binding.rollTwiceOutput.visibility = View.GONE
+                binding.parentOutputId.visibility = View.VISIBLE
+                binding.rollOnceOutput.text = randomFace.toString()
+            }else{
+                Toast.makeText(this, "Select dice", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
         binding.RollTwiceBtn.setOnClickListener{
@@ -61,6 +84,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+private fun storeUserDiceData(key: Int, value: Int){
+    val sharedPreference = getSharedPreferences("MyDicePreference", Context.MODE_PRIVATE)
+    val editor = sharedPreference.edit()
+    editor.putInt(key.toString(),value)
+    editor.apply()
+    listOfDice.add(value)
+}
+
+    private fun retrieveDataFromSharedPref(){
+        val sharedPrefs = getSharedPreferences("MyDicePreference", Context.MODE_PRIVATE)
+        val diceValues = sharedPrefs.all
+        for(i in 0 until diceValues.size -1){
+            val dice = sharedPrefs.getInt(i.toString(), 0)
+            listOfDice.add(dice)
+        }
+
+    }
 
 
 
