5e82f392c60b7a3ef70408e2b9e1d807aa901eea
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-04-16 23:32:49 -0400

    Added comments.

--- app/src/main/java/com/bikash/dicerollingapp/Dice.kt
+++ app/src/main/java/com/bikash/dicerollingapp/Dice.kt
@@ -4,27 +4,33 @@ import kotlin.random.Random
 
 class Dice {
 
+//    private varibales
     private var _numberOfSides: Int = 0
     private var _randomDiceSideFace: Int = 0
 
+
     constructor(){
+//        intial value of dice
         _numberOfSides = 6
         rollDice()
     }
 
 
-
+//secondary constructor
     constructor(numberOfSides: Int): this() {
         this._numberOfSides = numberOfSides
         rollDice()
     }
 
+//    getter funtion for getting randomDiceSideFace
     public fun getRandomSideUp():Int{
         return _randomDiceSideFace
     }
 
+//    function to roll dice
      fun rollDice() {
-         _randomDiceSideFace = Random.nextInt(1, _numberOfSides +1)
+//     get random number between 1 and number of sides
+    _randomDiceSideFace = Random.nextInt(1, _numberOfSides +1)
     }
 
 }
--- app/src/main/java/com/bikash/dicerollingapp/MainActivity.kt
+++ app/src/main/java/com/bikash/dicerollingapp/MainActivity.kt
@@ -12,20 +12,28 @@ import com.bikash.dicerollingapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var listOfDice: ArrayList<Int> = arrayListOf(4, 6, 8, 10, 12, 20)
+
+//    list of dice
+    private var listOfDice: ArrayList<Any> = arrayListOf()
+
+//    initial value of select dice
     private var selectedDice = 0
+    private var hintTextForSpinner: String = "Choose the dice size"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//        retrieve data from shared preference
         retrieveDataFromSharedPref()
 
-
+//create spinner adapter
         val spinnerAdapter =
             ArrayAdapter(this,  android.R.layout.simple_spinner_item, listOfDice)
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
         binding.diceTypesId.adapter = spinnerAdapter
 
+//        adding listener to spinner
         binding.diceTypesId.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
             override fun onItemSelected(
                 parent: AdapterView<*>?,
@@ -33,7 +41,13 @@ class MainActivity : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
-                selectedDice = binding.diceTypesId.selectedItem.toString().toInt()
+
+                val selectedSpinnerItem = binding.diceTypesId.selectedItem.toString()
+                if(selectedSpinnerItem != hintTextForSpinner){
+                    selectedDice = binding.diceTypesId.selectedItem.toString().toInt()
+                }else{
+                    binding.parentOutputId.visibility = View.GONE
+                }
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -44,19 +58,26 @@ class MainActivity : AppCompatActivity() {
 
         binding.CreateBtn.setOnClickListener(){
             val customDice = binding.customDiceId.text.toString()
-
+//if edit text is not empty check another condition
             if (customDice.isNotEmpty()){
                 if(customDice.toInt() == 0){
                     Toast.makeText(this, "The dice cannot be zero.",Toast.LENGTH_SHORT).show()
                 }
-                if(listOfDice.contains(customDice.toInt())){
+                else if(listOfDice.contains(customDice.toInt())){
                     Toast.makeText(this, "The dice entered is already created.",Toast.LENGTH_SHORT).show()
+                }else{
+                    val key = listOfDice.size - 1
+                    storeUserDiceData(key =key, value = customDice.toInt())
                 }
-                val key = listOfDice.size - 1
-                storeUserDiceData(key =key, value = customDice.toInt())
+
+            }else{ // show toast if text input is empty
+                Toast.makeText(this, "The text is empty.",Toast.LENGTH_SHORT).show()
             }
+//            clear the text input after the value is added to spinner
+            binding.customDiceId.text.clear()
         }
 
+//        added listener to rollonce btn
         binding.RollOnceBtn.setOnClickListener{
             if(selectedDice != 0){
                 val dice = Dice(numberOfSides = selectedDice)
@@ -71,38 +92,80 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+//        listenter added to rolltwice btn
         binding.RollTwiceBtn.setOnClickListener{
-            val dice = Dice(numberOfSides = selectedDice)
-            dice.rollDice()
-            val randomFace1 = dice.getRandomSideUp()
-            binding.parentOutputId.visibility = View.VISIBLE
-            binding.rollOnceOutput.text = randomFace1.toString()
-            dice.rollDice()
-            val randomFace2 = dice.getRandomSideUp()
-            binding.rollTwiceOutput.text = randomFace2.toString()
-            binding.rollTwiceOutput.visibility = View.VISIBLE
+//             if the selectd dice in spinner in not 0 run the code in the bidy
+            if(selectedDice != 0){
+//                create dice from Dice class
+                val dice = Dice(numberOfSides = selectedDice)
+
+//                roll dice
+                dice.rollDice()
+//                save the value
+                val randomFace1 = dice.getRandomSideUp()
+//                set the result view to visible
+                binding.parentOutputId.visibility = View.VISIBLE
+
+//                Display the first output
+                binding.rollOnceOutput.text = randomFace1.toString()
+//                roll dice again
+                dice.rollDice()
+//                save the value after the dice is rolled
+                val randomFace2 = dice.getRandomSideUp()
+//                Display the random side on second output
+                binding.rollTwiceOutput.text = randomFace2.toString()
+                binding.rollTwiceOutput.visibility = View.VISIBLE
+            }else{
+//                show toast if the selected dice is 0
+                Toast.makeText(this, "Select dice.", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 
 
+//    store data locally using shared preference
 private fun storeUserDiceData(key: Int, value: Int){
+
+// Get the shared preferences object for the app,
+// using a unique name to identify the preference file.
     val sharedPreference = getSharedPreferences("MyDicePreference", Context.MODE_PRIVATE)
-    val editor = sharedPreference.edit()
-    editor.putInt(key.toString(),value)
-    editor.apply()
+
+    if(value != 0){
+        val editor = sharedPreference.edit()
+
+        // Store the 'value' parameter in the shared preferences
+        // file using the 'key' parameter as the identifier.
+        editor.putInt(key.toString(),value)
+
+        // Apply the changes to the shared preferences file.
+        editor.apply()
+    }
+
+    //add the value parameter to the listofdice
     listOfDice.add(value)
 }
 
     private fun retrieveDataFromSharedPref(){
+        listOfDice.add(hintTextForSpinner)
+//        get shared preference file using the name of it
         val sharedPrefs = getSharedPreferences("MyDicePreference", Context.MODE_PRIVATE)
+
+        // get all the stored values from the share preference
         val diceValues = sharedPrefs.all
+
+        // loop through each kay-value pair
         for(i in 0 until diceValues.size -1){
+
             val dice = sharedPrefs.getInt(i.toString(), 0)
-            listOfDice.add(dice)
+
+            // add dice if it is not equal to 0.
+            if(dice != 0){
+                listOfDice.add(dice)
+            }
+
         }
 
     }
-
-
 
 }
